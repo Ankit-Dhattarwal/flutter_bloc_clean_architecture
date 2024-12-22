638ecfe13f437052221308e802465b8c6c6fb5d0
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-12-23 00:33:47 +0530

    basic setup flutter supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,6 @@
+
+class AppSecrets {
+  static const supabaseUrl = 'https://qnufqjmfymjhmezoueop.supabase.co';
+
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFudWZxam1meW1qaG1lem91ZW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4OTMyMDIsImV4cCI6MjA1MDQ2OTIwMn0.w8pIEPp87QHk_Z-prcE6KrP-4YlC0naTDmBFj2X31rg';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_clean_architecture/ankit-test/counter/counter_bloc.dart';
+import 'package:flutter_bloc_clean_architecture/core/secrets/app_secrets.dart';
 import 'package:flutter_bloc_clean_architecture/core/theme/theme.dart';
 import 'package:flutter_bloc_clean_architecture/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+      url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnonKey);
   runApp(const MyApp());
 }
 
@@ -19,9 +24,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: AppTheme.darkThemeMode,
-        home:  const LoginPage(),
+        home: const LoginPage(),
       ),
     );
   }
 }
-
