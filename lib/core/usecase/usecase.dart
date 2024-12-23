97d18016a53b7d51f90d2ba0dce7f915f231483f
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-12-24 01:05:25 +0530

    done signUp UseCase

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter_bloc_clean_architecture/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class UseCase<SuccessType, Params>{
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
