2c3ed73f0d47a9cd5b5518de3cca3c8befa7d0c8
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-12-29 01:06:21 +0530

    File image pick

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc_clean_architecture/core/theme/appColors.dart';
+import 'package:flutter_bloc_clean_architecture/core/utils/pick_image.dart';
 import 'package:flutter_bloc_clean_architecture/features/blog/presentation/widgets/blog_editor.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -19,6 +22,15 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
   final contentController = TextEditingController();
 
   List<String> selectedTopics = [];
+  File? image;
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,33 +46,53 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              DottedBorder(
-                color: AppColors.borderColor,
-                dashPattern: const [12, 6],
-                radius: const Radius.circular(10),
-                borderType: BorderType.RRect,
-                strokeCap: StrokeCap.round,
-                child: const SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              image != null
+                  ? GestureDetector(
+                onTap: selectImage,
+                    child: SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 15,
+                  )
+                  : GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: DottedBorder(
+                        color: AppColors.borderColor,
+                        dashPattern: const [12, 6],
+                        radius: const Radius.circular(10),
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        child: const SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                'Select your image',
+                                style: TextStyle(fontSize: 15),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        'Select your image',
-                        style: TextStyle(fontSize: 15),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+                    ),
               const SizedBox(
                 height: 20,
               ),
@@ -87,8 +119,10 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
                                       ? const WidgetStatePropertyAll(
                                           AppColors.gradient1)
                                       : null,
-                                  side: selectedTopics.contains(e) ? null : const BorderSide(
-                                      color: AppColors.borderColor),
+                                  side: selectedTopics.contains(e)
+                                      ? null
+                                      : const BorderSide(
+                                          color: AppColors.borderColor),
                                 ),
                               ),
                             ),
