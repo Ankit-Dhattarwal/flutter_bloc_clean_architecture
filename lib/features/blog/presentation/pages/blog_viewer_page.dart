e7d25cfde3360eb270d97b3a04109395ecd84c34
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2025-01-01 21:42:59 +0530

    blog viewer page and formatDate

---
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_clean_architecture/core/theme/appColors.dart';
+import 'package:flutter_bloc_clean_architecture/core/utils/calculate_reading_tiime.dart';
+import 'package:flutter_bloc_clean_architecture/core/utils/format_date.dart';
+import 'package:flutter_bloc_clean_architecture/features/blog/domain/entites/blog.dart';
+
+class BlogViewerPage extends StatelessWidget {
+  static route(Blog blog) => MaterialPageRoute(
+      builder: (context) => BlogViewerPage(
+            blog: blog,
+          ));
+  final Blog blog;
+
+  const BlogViewerPage({super.key, required this.blog});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                blog.title,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'By ${blog.posterName}',
+                style:
+                    const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                '${formatDateBydMMMYYYY(blog.updatedAt)}\t.\t${calculateReadingTime(blog.content)}\tmin',
+                style: const TextStyle(
+                    color: AppColors.greyColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(blog.imageUrl),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                blog.content,
+                style: const TextStyle(fontSize: 16, height: 2),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
